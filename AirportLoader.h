c8e567111c53d67b83ed96b499c28412c59c0abc
anoorullah <adnann2@illinois.edu>
2022-04-04 11:52:30 -0500

    Added functions to AirportLoader.h

--- AirportLoader.h
+++ AirportLoader.h
@@ -1,1 +1,26 @@
 #include "FileReader.h"
+#include <string>
+
+using namespace std;
+
+class AirportLoader {
+public:
+    AirportLoader(); // Constructor to set default values for all private variables
+    AirportLoader(string flight, string start, string end); 
+    // Constructor taking values of flight, starting airport, and ending airport
+    string getFlight() const; // Gets flight
+    void setFlight(string flight_); // Sets flight
+    string getStart() const; // Gets starting airport
+    void setStart(string start_point_); // Sets starting airport
+    string getEnd() const; // Gets ending airport
+    void setEnd(string end_point_); // Sets ending airport
+    double getWeight(); // Gets the weight of the edge (not const because will change for each edge)
+    void setWeight(double first_ns, double first_ew, double second_ns, double second_ew); // Setting spherical weights as suggested by Amit
+    // ns stands for north-south and ew stands for east-west
+
+private:
+    string flight;
+    string start_point;
+    string end_point;
+    double weight_value; // Stores the weight of each edge
+};
