aff4c561b6ef30ed6736e618303bc3f99e8513e0
anoorullah <adnann2@illinois.edu>
2022-04-04 12:38:39 -0500

    Finished initial functions for AirportLoader.cpp

--- AirportLoader.h
+++ AirportLoader.h
@@ -1,12 +1,14 @@
 #include "FileReader.h"
 #include <string>
+#include <vector>
+#include <map>
 
 using namespace std;
 
 class AirportLoader {
 public:
     AirportLoader(); // Constructor to set default values for all private variables
-    AirportLoader(string flight, string start, string end); 
+    AirportLoader(string flight_, string start_, string end_); 
     // Constructor taking values of flight, starting airport, and ending airport
     string getFlight() const; // Gets flight
     void setFlight(string flight_); // Sets flight
@@ -15,9 +17,11 @@ public:
     string getEnd() const; // Gets ending airport
     void setEnd(string end_point_); // Sets ending airport
     double getWeight(); // Gets the weight of the edge (not const because will change for each edge)
-    void setWeight(double first_ns, double first_ew, double second_ns, double second_ew); // Setting spherical weights as suggested by Amit
+    double findDistance(double first_ns, double first_ew, double second_ns, double second_ew);
+    // Finds spherical distance using Haversine formula
+    void setWeight(double first_ns, double first_ew, double second_ns, double second_ew); 
+    // Setting spherical weights as suggested by Amit
     // ns stands for north-south and ew stands for east-west
-
 private:
     string flight;
     string start_point;
